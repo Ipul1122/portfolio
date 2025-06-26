d9a637a031477c4f5b8a4715ef597b1d7e997c04
unknown <dori1233>
2025-06-26 22:38:06 +0700

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+export default function Hero() {
+return (
+    <section id="hero" className="h-screen flex items-center justify-center bg-blue-100 text-center">
+    <div>
+        <h1 className="text-5xl font-bold mb-4">Hi, I'm John Doe</h1>
+        <p className="text-xl mb-6">Frontend Developer | UI/UX Enthusiast</p>
+        <a href="#contact" className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700">
+        Hubungi Saya
+        </a>
+    </div>
+    </section>
+)
+}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // penting agar tailwind membaca file di src/
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
